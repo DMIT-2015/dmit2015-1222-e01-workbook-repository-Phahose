dbbab05e668a0290a11f3baefa67e0057784d7f0
Nicholas <nekwom1@nait.ca>
2023-01-27 19:46:21 -0700

    Servlet begin

---
+++ dmit2015-azure-webapp/src/main/java/dmit2015/servlet/QrCodeImageGeneratorServlet.java
@@ -0,0 +1,20 @@
+package dmit2015.servlet;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+import java.io.IOException;
+
+@WebServlet(name = "QrCodeImageGeneratorServlet", value = "/QrCodeImageGeneratorServlet")
+public class QrCodeImageGeneratorServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
